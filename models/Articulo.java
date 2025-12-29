2300a96837686a84086e08cfeb283aab1304b85e
Jairon Amilcar Taque Lopez <jtaque.gt@gmail.com>
2025-12-29 11:21:35 -0600

    Update Articulo.java

--- models/Articulo.java
+++ models/Articulo.java
@@ -10,7 +10,6 @@ public class Articulo {
     private int cantidad;
     private double precio;
 
-    // Constructores
     public Articulo() {}
 
     public Articulo(String codigo, int idProveedor, String nombre, String descripcion, int cantidad, double precio) {
@@ -23,31 +22,30 @@ public class Articulo {
     }
 
     public Articulo(int idArticulo, String codigo, int idProveedor, String nombre, String descripcion, int cantidad, double precio) {
-        this(codigo, idProveedor, nombre, descripcion, cantidad, precio); // reutiliza constructor anterior
+        this(codigo, idProveedor, nombre, descripcion, cantidad, precio);
         this.idArticulo = idArticulo;
     }
 
-    // Getters y Setters
     public int getIdArticulo() { return idArticulo; }
-    public void setIdArticulo(final int idArticulo) { this.idArticulo = idArticulo; }
+    public void setIdArticulo(int idArticulo) { this.idArticulo = idArticulo; }
 
     public String getCodigo() { return codigo; }
-    public void setCodigo(final String codigo) { this.codigo = codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
 
     public int getIdProveedor() { return idProveedor; }
-    public void setIdProveedor(final int idProveedor) { this.idProveedor = idProveedor; }
+    public void setIdProveedor(int idProveedor) { this.idProveedor = idProveedor; }
 
     public String getNombre() { return nombre; }
-    public void setNombre(final String nombre) { this.nombre = nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
     public String getDescripcion() { return descripcion; }
-    public void setDescripcion(final String descripcion) { this.descripcion = descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
 
     public int getCantidad() { return cantidad; }
-    public void setCantidad(final int cantidad) { this.cantidad = cantidad; }
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
 
     public double getPrecio() { return precio; }
-    public void setPrecio(final double precio) { this.precio = precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
 
     @Override
     public String toString() {
